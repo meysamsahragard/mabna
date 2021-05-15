d5bec7eee1d04ae2249538dbc8f851768cf5e6a5
Meysam Sahragard <meysam.sahragard@gmail.com>
2021-05-15 22:59:49 +0430

    login service removed

--- libs/shared/feature-login/src/lib/login/login.component.ts
+++ libs/shared/feature-login/src/lib/login/login.component.ts
@@ -1,23 +1,22 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 
-import { LoginService } from './login.service';
 import { AuthState, LogIn } from '@mabna/shared/store';
 import { User } from '@mabna/shared/model';
 
 @Component({
   selector: 'mabna-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  providers: [LoginService],
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  constructor(private store: Store<AuthState>) {}
+  constructor(private store: Store<AuthState>) {
+  }
 
   onSubmit(user: User): void {
     const payload = {
       username: user.username,
-      password: user.password,
+      password: user.password
     };
     this.store.dispatch(new LogIn(payload));
   }
